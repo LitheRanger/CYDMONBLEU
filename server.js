9a60e1e25b9804c6c356879ac16795ed3ccfebe5
LitheRanger <edmedina366@gmail.com>
2026-02-18 02:40:44 -0600

    Fix: agrega importación de pino

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cloudinary = require('cloudinary').v2;
+const pino = require('pino');
 const cors = require('cors');
 // Detectar si es PostgreSQL o MySQL
 const isPostgreSQL = (process.env.DATABASE_URL || '').includes('postgresql://');
